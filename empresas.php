47717a2730d1e266f54773b11f43a8cf40b091c5
Rafael Lozano <rafalozano284@gmail.com>
2020-06-20 23:25:32 -0500

    prueba 6

--- empresas.php
+++ empresas.php
@@ -138,12 +138,13 @@
                   $marca = $_POST['marca'];
                   $idTelefono = $_POST['idTelefono'];
                   $direccion = $_POST['direccion'];
-
-                  sqlsrv_query($conn, "INSERT INTO Marca (marca,idTelefono,direccion) values ('$marca','$idTelefono','$direccion')");
+                  $sql = "exec agregarMarca @marca='$marca', @idTelefono='$idTelefono', @direccion='$direccion'";
+                
                   //mysqli_query($conn, "INSERT INTO $tabla_db4 (marca,idTelefono,direccion) values ('$marca','$idTelefono','$direccion')");      
 
                   echo "<br<br><br><br<br><br><font><center><b><h1>¡Registro generado exitosamente!</h1></b></center></font>";
                   echo "<br<br><br><br<br><br><font><center><b><h3>La marca se registró de manera satisfactoria. Para generar otro reporte pulse el botón siguiente.</h3></b></center></font><br><br>";
+                  echo "$marca $idTelefono $direccion";
 
                 }
 
